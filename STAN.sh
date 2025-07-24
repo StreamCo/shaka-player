0bab57fcadee14ea143a8454094b8eb8248ee125
Haruki <haruki.takechi@gmail.com>
2025-07-24 22:52:41 +1000

    dist: Add custom builds for Play.Stan

---
+++ STAN.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+python3 build/build.py --name play +@complete -@cast -@cea -@devices -@mss -@offline -@polyfillForUI -@ui --force
+python3 build/build.py --name play-hls +@complete -@cast -@cea -@devices -@mss -@offline -@polyfillForUI -@ui -@dash --force
+python3 build/build.py --name play-dash +@complete -@cast -@cea -@devices -@mss -@offline -@polyfillForUI -@ui -@hls -@fairplay --force
