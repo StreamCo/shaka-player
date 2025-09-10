497926dbdc5515eed275a069801dd20c9ac7fe98
Haruki <haruki.takechi@gmail.com>
2025-09-10 22:25:09 +1000

    dist: Add v4.16.1 custom debug builds for Play.Stan

--- STAN.sh
+++ STAN.sh
@@ -3,3 +3,7 @@
 python3 build/build.py --name play +@complete -@cast -@cea -@devices -@mss -@offline -@polyfillForUI -@ui --force
 python3 build/build.py --name play-hls +@complete -@cast -@cea -@devices -@mss -@offline -@polyfillForUI -@ui -@dash --force
 python3 build/build.py --name play-dash +@complete -@cast -@cea -@devices -@mss -@offline -@polyfillForUI -@ui -@hls -@fairplay --force
+
+python3 build/build.py --name play +@complete -@cast -@cea -@devices -@mss -@offline -@polyfillForUI -@ui --force --mode debug
+python3 build/build.py --name play-hls +@complete -@cast -@cea -@devices -@mss -@offline -@polyfillForUI -@ui -@dash --force --mode debug
+python3 build/build.py --name play-dash +@complete -@cast -@cea -@devices -@mss -@offline -@polyfillForUI -@ui -@hls -@fairplay --force --mode debug
